11e5b73120f3a6d5ba640c3797e5cb26847d5895
Matthew Martinez <matt@matthews-imac.lan>
2017-09-22 08:23:27 -0500

    finish-assessment

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
 //---------------------------
+//  Javascript Assessment
+//---------------------------
+
+// For each section, read the instructions and add your code to the code area.
+// Wherever possible, use methods rather than global functions.
+// We're looking for conciseness, intuition and adherence to best practices. Good luck!
+
+
+
+
+//---------------------------
 //  Section 1
 //---------------------------
 
 
-// 1. Make an ajax call to openweathermap.org. Use their documentation to find out how to query their api.
-// 2. Use the appid 8690974851e3f54d7ce53bc8c6738558
-// 3. Make the API return a 5-day forecast for any city, with JSON as the mode and imperial as the units
-// 4. Log the result to the console
-// 5. Look for the description property for each day in the forecast and log that to the console as well
-
-
-//---------------------------
-//  Place Section 1 Code Here
-//---------------------------
-
-
-$.ajax({
-	'url': 'http://api.openweathermap.org/data/2.5/forecast/daily?id=524901&APPID=8690974851e3f54d7ce53bc8c6738558&mode=json&cnt=5&units=imperial',
-	'method': 'get',
-	'success': function(data) {
-		console.log(data);
-		$.each(data.list, function(i, item) {
-			console.log(item.weather[0].description);
-		});
-	}
-})
-
-
-//---------------------------
-//  End of Section 1 Code
-//---------------------------
+// Make an ajax call to openweathermap.org. Use their documentation to find out how to query their api.
+// Use the appid 8690974851e3f54d7ce53bc8c6738558
+// Make the API return a five day forecast for any city you'd like, with JSON as the mode and imperial as the units
+// Set your city in your query to a variable in the same scope as your ajax method.
 
 
 
@@ -38,16 +26,47 @@ $.ajax({
 //  Section 2
 //---------------------------
 
-1. 
+
+// Bootstrap has been added to index.html for this assessment.
+// For every day in your five day forecast, append a div with the class col-sm-4 to the existing row in index.html
+// In each div, add a header tag including the "main" property in that day's forecast
+// Next, include the day's icon in a responsive image. Refer to openweathermap's documentation to find out how to do this.
+// Next, include the day's weather description.
+// Finally, include the day's average temperature. The average temperature is not provided in the JSON response.
+
+
 
 
 //---------------------------
-//  Place Section 2 Code Here
+//  Section 3
+//---------------------------
+
+
+// Add a dropdown somewhere on the page with a list of three cities. Allow a user to click on a city and get an updated forecast for that city.
+// Update your method to allow users to choose between imperial and metric units.
+// Use the provided ajax loader .gif during ajax calls.
+
+
+
+
+//---------------------------
+//  Section 4
+//---------------------------
+
+
+// Add a method to the String object called grammarize, which capitalizes the first letter in a string and adds a period to the end of the string.
+// Run grammarize on all of your weather descriptions with each new ajax call.
+
+
+
+
+//---------------------------
+//  Place Code Here
 //---------------------------
 
 
 
+//---------------------------
+//  End of Code
+//---------------------------
 
-//---------------------------
-//  End of Section 2 Code
-//---------------------------
